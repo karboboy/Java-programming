757e62a0bc53ec897f3fb60705798d1429a47728
karboboy <55901895+karboboy@users.noreply.github.com>
2020-11-07 15:12:00 +0800

    Add files via upload

---
+++ java程序设计9.22/src/ComputeAreaWithConstant.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * @version 2-4
+ * @author LUO
+ *
+ */
+public class ComputeAreaWithConstant {
+
+	public static void main(String[] args) {
+		final double PI = 3.14159;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number for radius:");
+		double radius = input.nextDouble();
+		double area = radius * radius * PI;
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+
+	}
+
+}
